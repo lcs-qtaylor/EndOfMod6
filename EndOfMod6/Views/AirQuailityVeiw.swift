5ada2d0628ef4a65325455b840d6004e2f8ceb24
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-04-20 10:24:29 -0400

    Added network services and model, view and helper groups

---
+++ EndOfMod6/Views/AirQuailityVeiw.swift
@@ -0,0 +1,20 @@
+//
+//  AirQuailityVeiw.swift
+//  EndOfMod6
+//
+//  Created by Quin Taylor on 2023-04-20.
+//
+
+import SwiftUI
+
+struct AirQuailityVeiw: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AirQuailityVeiw_Previews: PreviewProvider {
+    static var previews: some View {
+        AirQuailityVeiw()
+    }
+}
---
+++ EndOfMod6/Views/AirQualView.swift
@@ -0,0 +1,32 @@
+//
+//  AirQualView.swift
+//  EndOfMod6
+//
+//  Created by Quin Taylor on 2023-04-20.
+//
+
+import SwiftUI
+//MARK: Stored Properties
+
+//MARK: Computed Properties
+
+struct AirQualView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                List{
+                    
+                }
+            }
+            
+            .navigationTitle("Air Quality")
+            
+        }
+    }
+}
+
+struct AirQualView_Previews: PreviewProvider {
+    static var previews: some View {
+        AirQualView()
+    }
+}
