47f912510681b75a03c561eb14324bfde58a7898
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-04-20 08:47:01 -0400

    Initial Commit

---
+++ EndOfMod6/EndOfMod6App.swift
@@ -0,0 +1,17 @@
+//
+//  EndOfMod6App.swift
+//  EndOfMod6
+//
+//  Created by Quin Taylor on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct EndOfMod6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
