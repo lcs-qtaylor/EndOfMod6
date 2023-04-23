6ba3760cb2241b082cbf58f6307b1978e0ae9208
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-04-22 20:49:34 -0400

    removed search view as we now have no use for it

--- EndOfMod6/Views/CatDetailView.swift
+++ EndOfMod6/Views/CatDetailView.swift
@@ -22,30 +22,30 @@ struct CatDetailView: View {
         NavigationView {
             VStack(alignment: .leading) {
                 
-             
-                    RemoteImageView(urlOfImageToShow: catToShow.previewUrl)
-                    
-                    .padding
-                        Button {
-                            
-                        } label: {
-                            
-                            Text("Favourite")
-                            
-                        }
                 
-                    }
-            }
-            .toolbar {
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: HistoryView()) {
-                        Image(systemName: "star.fill").font(.title)
-                            .foregroundColor(.yellow)
-                    }
+                RemoteImageView(urlOfImageToShow: catToShow.previewUrl)
+                
+                    .padding
+                Button {
+                    
+                } label: {
+                    
+                    Text("Favourite")
+                    
                 }
+                
+            }
         }
+//        .toolbar {
+//            ToolbarItemGroup(placement: .navigationBarTrailing) {
+//                NavigationLink(destination: HistoryView()) {
+//                    Image(systemName: "star.fill").font(.title)
+//                        .foregroundColor(.yellow)
+//                }
+//            }
+//        }
     }
-    
+}
     struct CatDetailView_Previews: PreviewProvider {
         static var previews: some View {
             CatDetailView(catToShow: exampleCat)
